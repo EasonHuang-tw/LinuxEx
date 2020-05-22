3129f0828ac3e70777286c927878c65402c51a3c
EricPwg <dz92286@gmail.com>
2020-05-22 20:07:02 +0800

    first commit

---
+++ delayedremovetag.cpp
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+const size_t DELAYED_SECONDS = 1;
+
+int main(int argc, char* const argv[]) {
+  ifstream fin;
+  if (argc == 2) {
+    fin.open(argv[1]);
+    if (fin)
+      cin.rdbuf(fin.rdbuf());
+    else {
+      cerr << "Unable to open file \"" << argv[1] << "\"" << endl;
+      return(1);
+    }
+  } else if (argc > 2) {
+    cerr << "Usage: " << argv[0] << ' ' << "[file]" << endl;
+    return(1);
+  }
+
+  bool in_tag = false;
+  char c;
+  while (cin.get(c)) {
+    switch (c) {
+      case '<':
+        in_tag = true;
+        break;
+      case '>':
+        in_tag = false;
+        break;
+      default:
+        if (!in_tag) {
+          cout.put(c);
+          if (c == '\n') {
+            cout.flush();
+            sleep(DELAYED_SECONDS);
+          }
+        }
+        break;
+    }
+  }
+  return 0;
+}
